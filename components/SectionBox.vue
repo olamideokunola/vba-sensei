4fe075097bfa46f0027dfcf4bf2eea49ee08abcb
root <olamideokunola@yahoo.com>
2021-04-24 11:37:40 +0100

    Video basic features

---
+++ components/SectionBox.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container flex flex-col px-12 pb-8 rounded-xl bg-white bg-opacity-50">
+        <div class="container flex flex-row items-center justify-between pt-6 ">
+          <h1 class="text-4xl text-gray font-extrabold">{{ sectionTitle }}</h1>
+          <slot name="actionbar"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import {
+  defineComponent,
+  useFetch,
+  useContext,
+  ref,
+  computed,
+} from 'nuxt-composition-api'
+
+export default {
+  components: {
+    
+  },
+  // layout: 'home',
+  props: {
+    title: String
+  },
+  setup(props, { root: { $fire } }) {
+    const sectionTitle = computed(() => props.title)
+    return {
+      sectionTitle
+    }
+  }
+}
+</script>
+{}
